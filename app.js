96a72bb3241b2b7410f69087430ea55e001b45c6
AARYA77-dev <keenu9826@gmail.com>
2026-02-04 19:30:48 +0530

    sql2

--- app.js
+++ app.js
@@ -35,10 +35,11 @@ app.use(
 );
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
 });
 
 // Connect to database
