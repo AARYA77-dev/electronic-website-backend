e51b7783731ccc8e54abb64ec4e444d4383a92ff
AARYA77-dev <keenu9826@gmail.com>
2026-02-06 11:30:05 +0530

    fetching by id

--- app.js
+++ app.js
@@ -34,24 +34,21 @@ app.use(
   })
 );
 
-// const db = mysql.createConnection({
-//   host: process.env.sql_host,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
+const db = mysql.createConnection({
+  host: process.env.sql_host,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-// // Connect to database
-// db.connect((err) => {
-//   if (err) {
-//     console.error('MySQL Connection Error:', err);
-//   } else {
-//     console.log('✅ Connected to MySQL');
-//   }
-// });
-
-const pool = mysql.createPool(process.env.DATABASE_URL);
-
+// Connect to database
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL Connection Error:', err);
+  } else {
+    console.log('✅ Connected to MySQL');
+  }
+});
 
 // Serve basic route (optional)
 app.get('/', (req, res) => {
